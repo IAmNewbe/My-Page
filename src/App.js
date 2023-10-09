25747d2e2ae067a4a9717a56c37134648430e547
IAmNewbe <ahlulhikam2222@gmail.com>
2023-10-09 09:44:44 +0700

    del unecesary

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
 import Footer from "./containers/Footer";
 import Header from "./components/Header";
 import Banner from "./components/Banner";
